7439db61342b63820e16095e9fc3734ca8a6f1cc
muriloschiler <muriloschiler.ls@gmail.com>
2022-08-31 15:00:46 -0300

    feat: Form invalid in MatSnackBar

--- Agenda.Angular/src/app/pages/login/login.component.ts
+++ Agenda.Angular/src/app/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
@@ -12,6 +13,7 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar
   ) {
     this.form = this.formBuilder.group({
       email: [null,[Validators.required,Validators.email]],
@@ -20,6 +22,15 @@ export class LoginComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+
+  isFormValid():boolean{
+    const valid = this.form.valid
+    if(valid){
+      this.form.markAllAsTouched();
+      this.snackBar.open("Há campos inválidos no formulário!", undefined, { duration: 3000 })
+    }
+    return valid;
   }
 
 }
--- Agenda.Angular/src/app/pages/login/login.module.ts
+++ Agenda.Angular/src/app/pages/login/login.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSnackBarModule
   ],
   declarations: [LoginComponent]
 })
